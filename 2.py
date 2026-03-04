067f84b0798ec12991d482fa3d090876ae78c584
Senjuuuuuu <143120307+Senjuuuuuu@users.noreply.github.com>
2026-03-04 14:40:25 +0700

    2.py

--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot("7618850095:AAHhkzF1y1G2NtOup195bcimW8jCVzNUivk")
+bot = telebot.TeleBot("8532835857:AAHFT2JXEGB0RCrCX2nhuRQErpjjCaUXcMo")
 
 @bot.message_handler(content_types=['voice'])
 def handle_voice_message(message):
